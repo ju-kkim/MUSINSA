e02792d43b8facfec6739d1179fb02504643f113
ju-kkim <wndus8198@naver.com>
2022-07-14 22:48:56 +0900

    Feat: SVG 아이콘 컴포넌트 구현

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,32 @@
+import { ReactComponent as Logo } from '../../assets/Icon/Logo.svg';
+import { ReactComponent as Search } from '../../assets/Icon/Search.svg';
+import { ReactComponent as Close } from '../../assets/Icon/Close.svg';
+import { ReactComponent as Reset } from '../../assets/Icon/Reset.svg';
+
+const iconComponents: iconComponentsType = {
+  Logo,
+  Search,
+  Close,
+  Reset,
+};
+
+export default function Icon({ icon, width, height, fill, stroke }: iconProps) {
+  const TargetIcon = iconComponents[icon];
+  if (!TargetIcon) {
+    throw new Error(`${TargetIcon} 컴포넌트를 찾을 수 없습니다.`);
+  }
+
+  return <TargetIcon width={width} height={height} fill={fill} stroke={stroke} />;
+}
+
+type iconProps = {
+  icon: string;
+  width?: string;
+  height?: string;
+  fill?: string;
+  stroke?: string;
+};
+
+type iconComponentsType = {
+  [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+};
